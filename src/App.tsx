2b385815c74850b99fd83b4be5918977c359a2c0
MrAmiD <nadymov2910@gmail.com>
2019-07-13 17:24:46 +0300

    Добавил библиотеки

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'typeface-roboto';
+import Button from '@material-ui/core/Button';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ReactTrainingClub!
-        </p>
+        <Button variant="contained" color="primary">
+          ReactTraining Club
+        </Button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
